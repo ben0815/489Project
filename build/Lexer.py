2fbdb7e13ea99cc7d6dfd9d2968039279c4770e8
ben0815 <ben0815@tamu.edu>
2017-10-18 10:12:22 -0500

    Imporiving Makefile. Minor changes to Lexer. Removing make.sh.

--- build/Lexer.py
+++ build/Lexer.py
@@ -2,25 +2,25 @@
 # Code for an extremely basic lexer. Needs minor tweaks to satisfy the spec details.
 
 tokens = {'principal' : 'PRINCIPAL', 'as' : 'AS', 'password' : 'PASSWORD', 'do' : 'DO',
-	  '***' : 'END', 'exit' : 'EXIT', 'return' : 'RETURN', '{' : 'LPAR', '}' : 'RPAR', 
-	  '[' : 'LBRACK', ']' : 'RBRACK', 'create' : 'CREATE', 'change' : 'CHANGE', 'password' : 	       
-	  'PASSWORD', 'set' : 'SET', 'append' : 'APPEND', 'to' : 'TO', 'with' : 'WITH', '=' : 		      
-	  'EQUALS', '.' : 'DOT', ',' : 'COMMA', '->' : 'ARROW', 'local' : 'LOCAL', 'foreach' : 		      
-	  'FOR', 'in' : 'IN', 'replacewith' : 'REPLACE', 'delegation' : 'DELEGATION', 'delete' : 	       
-	  'DELETE', 'default' : 'DEFAULT', 'read' : 'RIGHT', 'write' : 'RIGHT', 'append' : 	  	        
+	  '***' : 'END', 'exit' : 'EXIT', 'return' : 'RETURN', '{' : 'LPAR', '}' : 'RPAR',
+	  '[' : 'LBRACK', ']' : 'RBRACK', 'create' : 'CREATE', 'change' : 'CHANGE', 'password' :
+	  'PASSWORD', 'set' : 'SET', 'append' : 'APPEND', 'to' : 'TO', 'with' : 'WITH', '=' :
+	  'EQUALS', '.' : 'DOT', ',' : 'COMMA', '->' : 'ARROW', 'local' : 'LOCAL', 'foreach' :
+	  'FOR', 'in' : 'IN', 'replacewith' : 'REPLACE', 'delegation' : 'DELEGATION', 'delete' :
+	  'DELETE', 'default' : 'DEFAULT', 'read' : 'RIGHT', 'write' : 'RIGHT', 'append' :
 	  'RIGHT', 'delegate' : 'RIGHT'}
 
 def lexer(text):
     text = text.splitlines()
-    lexed = []	
+    lexed = []
 
     for line in text:
         lexed.append(['NEWLINE', '\n'])
         # TODO: Split on just spaces, not tabs
         word = ''
         i = 0
-        
-        # TODO: Verify that identifiers follow the spec. Alpha followed by alphanumeric 
+
+        # TODO: Verify that identifiers follow the spec. Alpha followed by alphanumeric
         # and underscore.
         while i < len(line):
             # Eat whitespace or append a new token
@@ -43,7 +43,7 @@ def lexer(text):
                         lexed.append([tokens[word], word])
                     else:
                         lexed.append(['IDENTIFIER', word])
-               
+
                 lexed.append([tokens[line[i]], line[i]])
                 word = ''
                 i += 1
@@ -56,14 +56,14 @@ def lexer(text):
                     if i == len(line):
                         print('FAILED')
                         return
-                    
+
                 i += 1
                 lexed.append(['STRING', word])
                 word = ''
             else:
                 word += line[i]
                 i += 1
-        
+
         # Get the last token
         if word != '':
             if word in tokens:
@@ -76,9 +76,3 @@ def lexer(text):
 
 # Testing using one of the examples pulled from the project spec
 lexer('as        principal admin password "admin" do\ncreate principal bob "B0BPWxxd"\nset x = "my string"\nset y ={f1=x,f2="field2"}\nset     delegation x admin read-> bob\nreturn y . f1\n***')
-
-
-	            
-	    
-		
-
