5dcaa3a1f39222db9fcf636962d5e56db7cdccd9
dairvin9 <dairvin9@build.tamu.edu>
2017-10-20 12:03:54 -0500

    added change password

--- build/Database.py
+++ build/Database.py
@@ -2,8 +2,18 @@ import copy
 
 # read write append delegate  (rwad)
 # TODO: RETURN STATUS CODES
-# TODO: make todo list of all functions to implement
+""" TODO: functions to implement:
 
+    create principal:            done
+    change password:             done
+    set (set_command):           started
+    append (append_command):     not started
+    local (local_commad):        not started
+    foreach:                     not started
+    set delegation:              started
+    delete delegation:           not started
+    default delegator:           done
+"""
 class Database:
 
     def __init__(self):
@@ -14,7 +24,7 @@ class Database:
         self.default_delegator = None
 
 
-    def create_default_delegator(self, caller, user):
+    def default_delegator(self, caller, user):
         if caller != "admin":
             raise SecurityError("To create a principal you must be admin.")
         if user not in self.user:
@@ -26,8 +36,8 @@ class Database:
         self.default_delegator['a'] = copy.deepcopy(self.user[user]['a'])
         self.default_delegator['d'] = copy.deepcopy(self.user[user]['d'])
         
-    def clear_local(self):
-        del(self.local)
+        return '{"status":"DEFAULT_DELEGATOR"}'
+
     
     def create_principal(self, caller, new_user, password):
         if caller != "admin":
@@ -39,10 +49,35 @@ class Database:
         self.user[new_user] = {}
         
         self.user[new_user]["password"] = password
-        self.user[new_user]["r"] = {}
-        self.user[new_user]["w"] = {}
-        self.user[new_user]["a"] = {}
-        self.user[new_user]["d"] = {}
+        
+        if self.default_delegator is None:
+            self.user[new_user]["r"] = set()
+            self.user[new_user]["w"] = set()
+            self.user[new_user]["a"] = set()
+            self.user[new_user]["d"] = set()
+        else:
+            self.user[new_user]["r"] = copy.deepcopy(self.default_delegator['r'])
+            self.user[new_user]["w"] = copy.deepcopy(self.default_delegator['w'])
+            self.user[new_user]["a"] = copy.deepcopy(self.default_delegator['a'])
+            self.user[new_user]["d"] = copy.deepcopy(self.default_delegator['d'])
+            
+        return '{"status":"CREATE_PRINCIPAL"}'
+        
+    def change_password(self, caller, user, password):
+        if caller != "admin" and user != caller:
+            raise SecurityError("Only admins can change other user's password")
+        if user not in self.user:
+            raise ParseError("User does not exist")
+        
+        self.user[user]["password"] = password
+        
+        return '{"status":"CHANGE_PASSWORD"}'
+        
+    # set is a keyword in python actually
+    def set_command(self, caller, var_name, value):
+        # i am not sure, does the user always have rights to local variables? 
+    
+        return '{"status":"SET"}'
         
     def set_delegation(self, caller, right, target, user_getting_rights):
         if user_getting_rights not in self.user or caller not in self.user:
@@ -56,7 +91,6 @@ class Database:
             pass
         # ill do it on sunday 
         
-    
     # helper function 
     def get_table(self, var_name):
         if var_names in self.local:
@@ -75,4 +109,6 @@ class Database:
         else:
             return None
 
-        
+    # helper function, call after everyloop
+    def clear_local(self):
+        del(self.local)
