b5b080fbdfc1b6df9a29a165fb73ecb908358673
justinbevolo <justinbevolo@tamu.edu>
2017-10-28 14:48:45 -0500

    Update TCPServer.py

--- build/TCPServer.py
+++ build/TCPServer.py
@@ -43,8 +43,6 @@ class TCPServer:
 
         print('Server started on port', self.port)
 
-        # INPUT PARSER
-        print(Parser.parse('as principal admin password "admin" do\nset records = "test"\nappend to records with { name = "mike", date = "1-1-90" }\nappend to records with { name = "dave", date = "1-1-85" }\nlocal names = records\nforeach rec in names replacewith rec.name\nreturn names\n***'))
         self.server.serve_forever()
 
     # Handle SIGTERM signals gracefully. For our purposes the server should
